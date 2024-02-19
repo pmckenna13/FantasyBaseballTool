397885e0fddc8b1c0e5f68d6a769a3dc58d56f4a
Cube <patrickmckennaeng@gmail.com>
2024-02-19 14:38:37 -0500

    Initial Project Commit

---
+++ src/main/java/com/example/fantasybaseballtool/FantasyBaseballToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.fantasybaseballtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyBaseballToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FantasyBaseballToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fantasybaseballtool/FantasyBaseballToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fantasybaseballtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyBaseballToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
